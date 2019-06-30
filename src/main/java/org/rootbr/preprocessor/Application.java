fd60a41fde9f70e3b3c37903806525597d0aa897
Aleksei Sheifler <a.a.shejfler@gmail.com>
2019-06-30 21:16:35 +0300

    init defined symbols

--- src/main/java/org/rootbr/preprocessor/Application.java
+++ src/main/java/org/rootbr/preprocessor/Application.java
@@ -1,5 +1,11 @@
 package org.rootbr.preprocessor;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Properties;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
@@ -10,33 +16,46 @@ import org.slf4j.LoggerFactory;
 
 public class Application {
   private static final Logger log = LoggerFactory.getLogger(Application.class);
+  public static final String PATH_TO_SOURCE = "path-to-source";
+  public static final String PATH_TO_DEFINED_SYMBOLS = "path-to-defined-symbols";
+  public static final String PATH_TO_OUTPUT = "path-to-output";
+  public static final Properties DEFINED_SYMBOLS = new Properties();
 
   public static void main(String[] args) {
     Options options = new Options()
         .addOption(Option.builder()
-            .longOpt("path-to-source")
+            .longOpt(PATH_TO_SOURCE)
             .hasArg().required()
             .desc("Путь к папке, в которой содержатся файлы с исходным кодом на языке C#")
             .build()
         ).addOption(Option.builder()
-            .longOpt("path-to-defined-symbols")
+            .longOpt(PATH_TO_DEFINED_SYMBOLS)
             .hasArg().required()
             .desc("Путь к тестовому файлу, содержащему набором символов (defined symbols)")
             .build()
         ).addOption(Option.builder()
-            .longOpt("path-to-output")
+            .longOpt(PATH_TO_OUTPUT)
             .hasArg().required()
             .desc("Путь к папке, в которую необходимо поместить обработанные файлы")
             .build()
         );
 
     try {
-      new DefaultParser().parse(options, args);
+      final var parse = new DefaultParser().parse(options, args);
+      try (InputStream input = new FileInputStream(parse.getOptionValue(PATH_TO_DEFINED_SYMBOLS))) {
+        // TODO add description
+        DEFINED_SYMBOLS.load(new InputStreamReader(input, Charset.forName("UTF-8")));
+      }
     } catch (ParseException e) {
-      log.error(e.getMessage());
-      System.err.println(e.getMessage());
       new HelpFormatter().printHelp("preprocessor-directives-utility", options);
-      System.exit(-1);
+      exitApplication(e.getMessage());
+    } catch (IOException e) {
+      exitApplication(e.getMessage());
     }
+  }
+
+  private static void exitApplication(String message) {
+    log.error(message);
+    System.exit(-1);
   }
 }
