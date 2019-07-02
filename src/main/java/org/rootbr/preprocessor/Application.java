62380fbe84975f6a47fe9957dc8231b19d422e23
Aleksei Sheifler <a.a.shejfler@gmail.com>
2019-07-02 22:27:54 +0300

    add matchers for different expresion

--- src/main/java/org/rootbr/preprocessor/Application.java
+++ src/main/java/org/rootbr/preprocessor/Application.java
@@ -11,9 +11,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -33,8 +35,14 @@ public class Application {
   private static final String PATH_TO_DEFINED_SYMBOLS = "path-to-defined-symbols";
   private static final String PATH_TO_OUTPUT = "path-to-output";
   private static DefinedSymbols definedSymbols;
-  private static final Pattern pattern =
+  private static final Pattern patternIf =
       Pattern.compile("^[\\s\\t]*#if[\\s\\t]+(!*?)(\\w+?)[\\s\\t]*$");
+  private static final Pattern patternElseIf =
+      Pattern.compile("^[\\s\\t]*#elif[\\s\\t]+(!*?)(\\w+?)[\\s\\t]*$");
+  private static final Pattern patternElse =
+      Pattern.compile("^[\\s\\t]*#else[\\s\\t]*$");
+  private static final Pattern patternEndIf =
+      Pattern.compile("^[\\s\\t]*#endif[\\s\\t]*$");
 
   public static void main(String[] args) throws IOException {
 
@@ -52,14 +60,7 @@ public class Application {
       walk.filter(Files::isRegularFile)
           .forEach(f -> pool.execute(() -> {
                 final var to = rootPathDestination.resolve(f.subpath(rootPathSource.getNameCount(), f.getNameCount()));
-                Path parentDir = to.getParent();
-                if (!Files.exists(parentDir)) {
-                  try {
-                    Files.createDirectories(parentDir);
-                  } catch (IOException e) {
-                    log.error(e.getMessage(), e);
-                  }
-                }
+                createParentFolderIfNotExist(to);
 
                 if (f.toFile().getAbsolutePath().endsWith(".cs")) {
                   try {
@@ -85,6 +86,17 @@ public class Application {
     awaitPool(pool);
   }
 
+  private static void createParentFolderIfNotExist(Path to) {
+    Path parentDir = to.getParent();
+    if (!Files.exists(parentDir)) {
+      try {
+        Files.createDirectories(parentDir);
+      } catch (IOException e) {
+        log.error(e.getMessage(), e);
+      }
+    }
+  }
+
   private static String detectCharset(Path f) {
     try {
       return UniversalDetector.detectCharset(f);
@@ -96,25 +108,50 @@ public class Application {
   }
 
   private static void processingFile(final Path f, final Path t, Charset charset) {
-    try (Stream<String> stream = Files.lines(f, charset)) {
-      final BufferedWriter writer = Files.newBufferedWriter(t, charset, StandardOpenOption.CREATE);
-      stream.forEachOrdered(s -> {
-        try {
-          writer.write(s);
-          writer.newLine();
-        } catch (IOException e) {
-          log.error(e.getMessage(), e);
-        }
-        Matcher matcher = pattern.matcher(s);
-        if (matcher.find()) {
-          final var numberOfNegation = matcher.group(1).length();
-          final var key = matcher.group(2);
-          if (is(key, numberOfNegation)) {
-            log.info(s);
+
+    try (BufferedWriter writer = Files.newBufferedWriter(t, charset, StandardOpenOption.CREATE)) {
+      final AtomicBoolean needBuffered = new AtomicBoolean(false);
+      final ArrayList<String> lines = new ArrayList<>();
+      try (Stream<String> stream = Files.lines(f, charset)) {
+        stream.forEachOrdered(s -> {
+          try {
+            Matcher matcher;
+            if (!needBuffered.get()) {
+              matcher = patternIf.matcher(s);
+
+              if (matcher.find()) {
+                final var numberOfNegation = matcher.group(1).length();
+                final var key = matcher.group(2);
+                if (is(key, numberOfNegation)) {
+                  log.info(s);
+                  needBuffered.set(true);
+                }
+              }
+            } else {
+              matcher = patternElseIf.matcher(s);
+              matcher = patternElse.matcher(s);
+              matcher = patternEndIf.matcher(s);
+            }
+            if (matcher.find() && !needBuffered.get()) {
+              final var numberOfNegation = matcher.group(1).length();
+              final var key = matcher.group(2);
+              if (is(key, numberOfNegation)) {
+                log.info(s);
+                needBuffered.set(true);
+              }
+            } else if (needBuffered.get() &&) {
+              lines.add(s);
+            } else {
+              writer.write(s);
+              writer.newLine();
+            }
+
+          } catch (IOException e) {
+            log.error(e.getMessage(), e);
           }
-        }
-      });
-      writer.close();
+
+        });
+      }
     } catch (IOException e) {
       log.warn(e.getMessage(), e);
     }
